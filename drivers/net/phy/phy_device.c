63c0d74096ea0837098641ac7d2a280113170372
Alex Elder <elder@linaro.org>
2017-07-30 08:09:38 +0100

    drivers/thermal: add other possible no-device PHY ids
    Apparently the PHY on Poplar might return an ID of 0xffff0000 or
    0x0000ffff when there is no device present.  This implements that
    logic.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -365,8 +365,12 @@ struct phy_device *get_phy_device(struct mii_bus *bus, int addr, bool is_c45)
 	if (r)
 		return ERR_PTR(r);
 
-	/* If the phy_id is mostly Fs, there is no device there */
-	if ((phy_id & 0x1fffffff) == 0x1fffffff)
+	/*
+	 * If the phy_id is mostly Fs, there is no device there
+	 * Or the phy_id is 0000 or ffff
+	 */
+	if (((phy_id & 0x1fffffff) == 0x1fffffff) || (phy_id == 0) ||
+	    (phy_id == 0xffff0000) || (phy_id == 0x0000ffff))
 		return NULL;
 
 	return phy_device_create(bus, addr, phy_id, is_c45, &c45_ids);
