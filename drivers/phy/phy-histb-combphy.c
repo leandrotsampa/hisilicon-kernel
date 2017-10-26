e304ef6b63c2bbb2b59e6eddd60d464a72c06e64
Shawn Guo <shawn.guo@linaro.org>
2017-10-26 09:51:28 +0800

    phy: histb-combphy: align with upstreaming v3 driver
    The updates includes:
    
     - Make combphy device be child of peripheral controller and use 'reg'
       property for mapping combphy configuration registers.
     - Kill "hisilicon,peripheral-syscon" property, since parent node is
       just the syscon controller now.
     - Check combphy id to handle the quirk that combphy0 can not configure
       mode but always works in USB3 mode.
     - Unify phy .init and .exit hooks for different combphy instances and
       work modes, as the only quirk we need to handle is that combphy0 can
       only work in USB3 mode.
     - Better naming for clock and reset, 'ref' to 'ref_clk', 'por' to
       'por_rst'.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- drivers/phy/phy-histb-combphy.c
+++ drivers/phy/phy-histb-combphy.c
@@ -22,148 +22,130 @@
 #include <linux/reset.h>
 #include <dt-bindings/phy/phy.h>
 
-#define PERI_CTRL			0x0008
-#define PERI_COMBPHY1_CFG		0x0858
-
+#define PERI_CTRL			0x8
 #define COMBPHY1_MODE_MASK		GENMASK(12, 11)
-#define COMBPHY_MODE_SHIFT		11
+#define COMBPHY1_MODE_SHIFT		11
 #define COMBPHY_MODE_PCIE		0
-#define COMBPHY_MODE_USB		1
+#define COMBPHY_MODE_USB3		1
 #define COMBPHY_MODE_SATA		2
 
-#define COMBPHY1_BYPASS_CODEC_MASK	BIT(31)
-#define COMBPHY1_BYPASS_CODEC_VAL	(1 << 31)
-
-#define COMBPHY1_CLKREF_OUT_OEN_MASK	BIT(0)
-#define COMBPHY1_CLKREF_OUT_OEN_VAL	(1 << 0)
-
-#define NANO_TEST_WRITE			GENMASK(24, 24)
-#define NANO_TEST_DATA			GENMASK(23, 20)
-#define NANO_TEST_ADDR			GENMASK(16, 12)
+#define COMBPHY_CFG_REG			0x0
+#define COMBPHY_BYPASS_CODEC		BIT(31)
+#define COMBPHY_TEST_WRITE		BIT(24)
+#define COMBPHY_TEST_DATA_SHIFT		20
+#define COMBPHY_TEST_DATA_MASK		GENMASK(23, 20)
+#define COMBPHY_TEST_ADDR_SHIFT		12
+#define COMBPHY_TEST_ADDR_MASK		GENMASK(16, 12)
+#define COMBPHY_CLKREF_OUT_OEN		BIT(0)
 
 struct histb_combphy_priv {
-	u32 mode;
-	struct regmap *peri;
-	struct clk *ref;
+	void __iomem *mmio;
+	struct regmap *syscon;
+	struct reset_control *por_rst;
+	struct clk *ref_clk;
 	struct phy *phy;
-	struct reset_control *por;
+	int phy_mode;
+	int phy_id;
 };
 
-static void nano_register_write(struct regmap *peri, u32 addr,
-				u32 offset, u32 value)
+static void nano_register_write(struct histb_combphy_priv *priv,
+				u32 addr, u32 data)
 {
+	void __iomem *reg = priv->mmio + COMBPHY_CFG_REG;
 	u32 val;
-	int ret;
 
-	ret = regmap_read(peri, addr, &val);
-	val &= ~NANO_TEST_ADDR;
-	val &= ~NANO_TEST_DATA;
-	val |= (offset << 12);
-	val |= (value << 20);
-	ret |= regmap_write(peri, addr, val);
+	/* Set up address and data for the write */
+	val = readl(reg);
+	val &= ~COMBPHY_TEST_ADDR_MASK;
+	val |= addr << COMBPHY_TEST_ADDR_SHIFT;
+	val &= ~COMBPHY_TEST_DATA_MASK;
+	val |= data << COMBPHY_TEST_DATA_SHIFT;
+	writel(val, reg);
 
-	ret = regmap_read(peri, addr, &val);
-	val &= ~NANO_TEST_WRITE;
-	ret |= regmap_write(peri, addr, val);
-
-	ret = regmap_read(peri, addr, &val);
-	val |= NANO_TEST_WRITE;
-	ret |= regmap_write(peri, addr, val);
+	/* Flip strobe control to trigger the write */
+	val &= ~COMBPHY_TEST_WRITE;
+	writel(val, reg);
+	val |= COMBPHY_TEST_WRITE;
+	writel(val, reg);
 }
 
-static int histb_pcie_phy_init(struct histb_combphy_priv *priv)
+static int histb_combphy_set_mode(struct histb_combphy_priv *priv)
 {
-	struct regmap *peri = priv->peri;
-	int ret;
+	struct regmap *syscon = priv->syscon;
+	u32 mode;
 
-	/* set to pcie mode */
-	regmap_update_bits(peri, PERI_CTRL, COMBPHY1_MODE_MASK,
-			    COMBPHY_MODE_PCIE << COMBPHY_MODE_SHIFT);
-
-	regmap_update_bits(peri, PERI_COMBPHY1_CFG,
-			    COMBPHY1_BYPASS_CODEC_MASK,
-			    ~COMBPHY1_BYPASS_CODEC_VAL);
-
-	ret = clk_prepare_enable(priv->ref);
-	if (ret) {
-		dev_err(&priv->phy->dev, "clk_prepare_enable fail!\n");
-		return ret;
+	switch (priv->phy_mode) {
+	case PHY_TYPE_SATA:
+		mode = COMBPHY_MODE_SATA;
+		break;
+	case PHY_TYPE_PCIE:
+		mode = COMBPHY_MODE_PCIE;
+		break;
+	case PHY_TYPE_USB3:
+		mode = COMBPHY_MODE_USB3;
+		break;
+	default:
+		return -EINVAL;
 	}
 
-	reset_control_deassert(priv->por);
-
-	regmap_update_bits(peri, PERI_COMBPHY1_CFG,
-			    COMBPHY1_CLKREF_OUT_OEN_MASK,
-			    COMBPHY1_CLKREF_OUT_OEN_VAL);
-
-	/* need to wait for EP clk stable */
-	mdelay(5);
-
-	nano_register_write(peri, PERI_COMBPHY1_CFG, 0x1, 0x8);
-	nano_register_write(peri, PERI_COMBPHY1_CFG, 0xc, 0x9);
-	nano_register_write(peri, PERI_COMBPHY1_CFG, 0x1a, 0x4);
-
-	return 0;
-}
-
-static int histb_pcie_phy_exit(struct histb_combphy_priv *priv)
-{
-	regmap_update_bits(priv->peri, PERI_COMBPHY1_CFG,
-				COMBPHY1_CLKREF_OUT_OEN_MASK,
-				~COMBPHY1_CLKREF_OUT_OEN_VAL);
-	reset_control_deassert(priv->por);
-	clk_disable_unprepare(priv->ref);
-
-	return 0;
-}
-
-static int histb_usb_phy_init(struct histb_combphy_priv *priv)
-{
-	int ret;
-
-	ret = clk_prepare_enable(priv->ref);
-	if (ret) {
-		dev_err(&priv->phy->dev, "clk_prepare_enable fail!\n");
-		return ret;
-	}
-	reset_control_deassert(priv->por);
-	mdelay(1);
-
-	return 0;
-}
-
-static int histb_usb_phy_exit(struct histb_combphy_priv *priv)
-{
-	reset_control_deassert(priv->por);
-	clk_disable_unprepare(priv->ref);
-
-	return 0;
+	return regmap_update_bits(syscon, PERI_CTRL, COMBPHY1_MODE_MASK,
+				  mode << COMBPHY1_MODE_SHIFT);
 }
 
 static int histb_combphy_init(struct phy *phy)
 {
 	struct histb_combphy_priv *priv = phy_get_drvdata(phy);
-	int ret = -1;
+	u32 val;
+	int ret;
 
-	if (priv->mode == PHY_TYPE_PCIE)
-		ret = histb_pcie_phy_init(priv);
-	else if (priv->mode == PHY_TYPE_USB3)
-		ret = histb_usb_phy_init(priv);
+	/* PHY0 doesn't support mode setting */
+	if (priv->phy_id != 0) {
+		ret = histb_combphy_set_mode(priv);
+		if (ret)
+			return ret;
+	}
 
-	return ret;
+	/* Clear bypass bit to enable encoding/decoding */
+	val = readl(priv->mmio + COMBPHY_CFG_REG);
+	val &= ~COMBPHY_BYPASS_CODEC;
+	writel(val, priv->mmio + COMBPHY_CFG_REG);
+
+	ret = clk_prepare_enable(priv->ref_clk);
+	if (ret)
+		return ret;
+
+	reset_control_deassert(priv->por_rst);
+
+	/* Enable EP clock */
+	val = readl(priv->mmio + COMBPHY_CFG_REG);
+	val |= COMBPHY_CLKREF_OUT_OEN;
+	writel(val, priv->mmio + COMBPHY_CFG_REG);
+
+	/* Need to wait for EP clock stable */
+	mdelay(5);
+
+	/* Configure nano phy registers as suggested by vendor */
+	nano_register_write(priv, 0x1, 0x8);
+	nano_register_write(priv, 0xc, 0x9);
+	nano_register_write(priv, 0x1a, 0x4);
+
+	return 0;
 }
 
 static int histb_combphy_exit(struct phy *phy)
 {
 	struct histb_combphy_priv *priv = phy_get_drvdata(phy);
-	int ret = 0;
+	u32 val;
 
-	if (priv->mode == PHY_TYPE_PCIE)
-		ret = histb_pcie_phy_exit(priv);
-	else if (priv->mode == PHY_TYPE_USB3)
-		ret = histb_usb_phy_exit(priv);
+	/* Disable EP clock */
+	val = readl(priv->mmio + COMBPHY_CFG_REG);
+	val &= ~COMBPHY_CLKREF_OUT_OEN;
+	writel(val, priv->mmio + COMBPHY_CFG_REG);
 
-	return ret;
+	reset_control_assert(priv->por_rst);
+	clk_disable_unprepare(priv->ref_clk);
+
+	return 0;
 }
 
 static const struct phy_ops histb_combphy_ops = {
@@ -178,15 +160,20 @@ static struct phy *histb_combphy_xlate(struct device *dev,
 	struct histb_combphy_priv *priv = dev_get_drvdata(dev);
 
 	if (args->args_count < 1) {
-		dev_err(dev, "DT did not pass correct no of args\n");
-		return ERR_PTR(-ENODEV);
+		dev_err(dev, "invalid number of arguments\n");
+		return ERR_PTR(-EINVAL);
 	}
 
-	priv->mode = args->args[0];
+	priv->phy_mode = args->args[0];
 
-	if (priv->mode > PHY_TYPE_USB3) {
-		dev_err(dev, "DT did not pass correct phy mode\n");
-		return ERR_PTR(-ENODEV);
+	if (priv->phy_mode < PHY_TYPE_SATA || priv->phy_mode > PHY_TYPE_USB3) {
+		dev_err(dev, "invalid phy mode argument\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	if (priv->phy_id == 0 && priv->phy_mode != PHY_TYPE_USB3) {
+		dev_err(dev, "PHY0 works in USB3 mode only\n");
+		return ERR_PTR(-EINVAL);
 	}
 
 	return priv->phy;
@@ -194,31 +181,44 @@ static struct phy *histb_combphy_xlate(struct device *dev,
 
 static int histb_combphy_probe(struct platform_device *pdev)
 {
+	struct phy_provider *phy_provider;
 	struct device *dev = &pdev->dev;
 	struct histb_combphy_priv *priv;
-	struct phy_provider *phy_provider;
+	struct device_node *np = dev->of_node;
+	struct resource *res;
+	int ret;
 
 	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
 		return -ENOMEM;
 
-	priv->peri = syscon_regmap_lookup_by_phandle(dev->of_node,
-				"hisilicon,peripheral-syscon");
-	if (IS_ERR(priv->peri)) {
-		dev_err(dev, "failed to find peri_ctrl regmap\n");
-		return PTR_ERR(priv->peri);
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	priv->mmio = devm_ioremap_resource(dev, res);
+	if (IS_ERR(priv->mmio)) {
+		ret = PTR_ERR(priv->mmio);
+		return ret;
 	}
 
-	priv->ref = devm_clk_get(dev, NULL);
-	if (IS_ERR(priv->ref)) {
-		dev_err(dev, "failed to find ref clk\n");
-		return PTR_ERR(priv->ref);
+	priv->syscon = syscon_node_to_regmap(np->parent);
+	if (IS_ERR(priv->syscon)) {
+		dev_err(dev, "failed to find peri_ctrl syscon regmap\n");
+		return PTR_ERR(priv->syscon);
 	}
 
-	priv->por = devm_reset_control_get(dev, NULL);
-	if (IS_ERR(priv->por)) {
-		dev_err(dev, "failed to por reset\n");
-		return PTR_ERR(priv->por);
+	priv->phy_id = of_alias_get_id(np, "combphy");
+	if (priv->phy_id < 0)
+		priv->phy_id = 0;
+
+	priv->ref_clk = devm_clk_get(dev, NULL);
+	if (IS_ERR(priv->ref_clk)) {
+		dev_err(dev, "failed to find ref clock\n");
+		return PTR_ERR(priv->ref_clk);
+	}
+
+	priv->por_rst = devm_reset_control_get(dev, NULL);
+	if (IS_ERR(priv->por_rst)) {
+		dev_err(dev, "failed to get poweron reset\n");
+		return PTR_ERR(priv->por_rst);
 	}
 
 	priv->phy = devm_phy_create(dev, NULL, &histb_combphy_ops);
