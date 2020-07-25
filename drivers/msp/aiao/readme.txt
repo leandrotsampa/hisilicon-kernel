2e5085f0cccbee43c32e6813af2fda5bab98aab5
Leandro Tavares de Melo <leandrotsampa@yahoo.com.br>
2020-07-25 11:20:08 -0300

    Enabled FB FillRect/CopyArea/ImageBlit on defconfig. Removed all unecessary data from Hisilicon MSP/COMMON.

--- drivers/msp/aiao/readme.txt
+++
@@ -1,2 +0,0 @@
-aiao_sw_vx_x: arm ao ������֧
-aiao_hw_vx_x: dsp ao ������֧
--- drivers/msp/gpu/mali700/drivers/gpu/arm/midgard/platform/vexpress/mali_gpu_proc.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MALI_GPU_PROC_H__
-#define __MALI_GPU_PROC_H__
-
-int gpu_proc_create(void);
-int gpu_proc_destroy(void);
-
-#endif
